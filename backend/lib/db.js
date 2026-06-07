34a9db4a2745cb4d3976542f89da545b011d8559
Ismael <ismaeljaycarpio1992@gmail.com>
2026-06-07 21:25:32 +0800

    refactor db.js connection error handling for consistency

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MondoDB connected: ${conn.connection.host}`);
-
-    }catch(error){
-        console.log("Error connecting to MongoDB", error.message);
-        process.exit(1); //FAILURE
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MondoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+    process.exit(1); //FAILURE
+  }
 };
